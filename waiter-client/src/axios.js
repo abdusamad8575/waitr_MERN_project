7c0fd71ccaa3876a7b0fe4aaae93769045eeee03
abdusamad8575 <samadpts786313@gmail.com>
2023-10-11 21:32:04 +0530

    testing process

--- waiter-client/src/axios.js
+++ waiter-client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 
-const baseURL = "http://localhost:8000";
-// const baseURL = "http://18.234.93.80:8000";
+// const baseURL = "http://localhost:8000";
+const baseURL = "http://54.162.107.27:8000";
 const axiosInstance = axios.create({
   baseURL: baseURL,
   // timeout: 10000,
