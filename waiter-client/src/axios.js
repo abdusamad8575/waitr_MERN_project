3ecf19d0bc32b01b181350e5030a24264dfc7b84
abdusamad8575 <samadpts786313@gmail.com>
2023-10-25 19:44:33 +0530

    hosting processing

--- waiter-client/src/axios.js
+++ waiter-client/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const baseURL = "http://localhost:8000";
 // const baseURL = "https://waitr-project.onrender.com";
-const baseURL = "https://mcart.site";
+const baseURL = "https://waitr.shop";
 const axiosInstance = axios.create({
   baseURL: baseURL,
   // timeout: 10000,
